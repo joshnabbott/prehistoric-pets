4a6d640aa81a48b0f63320019f66f73b0950fdd1
Joshua Abbott <joshnabbott@mac.com>
2009-01-12 00:18:54 -0800

    fixed a little error in the category form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   acts_as_list :scope => :parent_id
-  acts_as_tree
+  acts_as_tree :order => :parent_id
+  validates_presence_of :name
+  validates_uniqueness_of :name, :allow_nil => true
 end
