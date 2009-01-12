08b3fc0ef35794df9b89551266ef12f94707a09e
Joshua Abbott <joshnabbott@mac.com>
2009-01-12 00:00:45 -0800

    removing frontend categories. those aren't needed. at leat not yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :categories
-
-  map.admin 'admin/', :controller => 'users'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
@@ -9,6 +6,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resource :session
 
+  # Admin
+  map.admin 'admin/', :controller => 'categories'
   map.namespace(:admin) do |admin|
     admin.resources :categories
   end
