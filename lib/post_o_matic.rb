2e86218fe632864e33fc8ee7029dcd692a08f12f
Joshua Abbott <joshnabbott@mac.com>
2009-01-25 21:10:24 -0800

    added very simple validation to whether or not a post-o-matic post actually gets posted. the method 'post_ad' returns true or false depending on whether or not the ad gets posted

--- lib/post_o_matic.rb
+++ lib/post_o_matic.rb
@@ -25,7 +25,8 @@ module PostOMatic
       :corn_snakes                => 'http://market.kingsnake.com/list.php?cat=65',
       :gray_banded_kingsnakes     => 'http://market.kingsnake.com/list.php?cat=29',
       :other_kings_and_milksnakes => 'http://market.kingsnake.com/list.php?cat=59',
-      :other_snakes               => 'http://market.kingsnake.com/list.php?cat=6'
+      :other_snakes               => 'http://market.kingsnake.com/list.php?cat=6',
+      :venomous                   => 'http://market.kingsnake.com/list.php?cat=40'
     }
 
     def post_ad(options = {}) # options = { :ad_duration => '5 days' }
@@ -45,51 +46,50 @@ module PostOMatic
     end
 
     def submit_ad(page)
-      page          = @agent.get(URLS[:other_snakes]) # URLS[self.category.to_sym]
+      page          = @agent.get(URLS[self.category.to_sym]) # URLS[self.category.to_sym]
       form          = page.forms[1]
       return false if form.nil?
       form.subject  = name
       form.descript = description
-      form.imageurl = "http:/prehistoricpets.com/products/#{to_param}.jpg"
-      # page        = form.submit # works
-      validate_post_success(form)
+      # form.imageurl = "http:/prehistoricpets.com/products/#{to_param}.jpg" # is this breaking it when I try to post?
+      page          = form.submit
+      validate_post_success(page)
     end
 
     def validate_post_success(page)
-      # if page.something_on_the_page.eql?('bla bla bla')
-      #   return true
-      # else
-      #   return false
-      # end
-      return true
+      if page.title =~ /Thank You/
+        return true
+      else
+        return false
+      end
     end
   end
 end
 # Keep this for testing:
-# class Product
-#   include PostOMatic::KingSnake
-#   attr_accessor :name, :description, :imageurl
-# 
-#   def initialize
-#     @name = "Awesome Ball Python"
-#     @description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-#   end
-# 
-#   def category
-#     "ball_pythons"
-#   end
-# 
-#   def name
-#     @name
-#   end
-# 
-#   def description
-#     @description
-#   end
-# 
-#   def to_param
-#     name
-#   end
-# end
-# p = Product.new
-# puts p.post_ad
+class Product
+  include PostOMatic::KingSnake
+  attr_accessor :name, :description, :imageurl
+
+  def initialize
+    @name = "Test"
+    @description = "This is a test."
+  end
+
+  def category
+    "venomous"
+  end
+
+  def name
+    @name
+  end
+
+  def description
+    @description
+  end
+
+  def to_param
+    name
+  end
+end
+p = Product.new
+puts p.post_ad
