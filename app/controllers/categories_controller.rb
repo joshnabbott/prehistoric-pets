283173e0141688d4be5fabaf36e5038a824540e3
Joshua Abbott <joshnabbott@joshua-abbotts-macbook-pro.local>
2009-03-02 21:47:47 -0800

    made dynamic categories using is_price_specific field

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,10 +2,10 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by_permalink(params[:categories].last)
     raise Error404 unless @category
-    # if @category.is_price_specific
-    #   @product = Product.by_price(@category.starting_price,@category.ending_price)
-    # else
+    if @category.is_price_specific
+      @products = Product.by_price(@category.starting_price,@category.ending_price)
+    else
       @products = @category.products.active
-    # end
+    end
   end
 end
