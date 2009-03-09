d9d2eac329772d08b2d418f898b81bda3c26aab9
Joshua Abbott <joshnabbott@joshua-abbotts-macbook-pro.local>
2009-03-08 20:44:13 -0700

    added a js function to dynamically display the items in cart count since pages are being cached

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
   $('a[rel*=facebox]').facebox();
   $("ul#announcement_tabs").tabs();
+
   // toggle videos on homepage
   $('div#videos ul#thumbnails > li a').click(function() {
     text = '<embed width="328" height="254" type="application/x-shockwave-flash" flashvars="m='+this.id+'&v=2&type=video" src="http://lads.myspace.com/videos/vplayer.swf"/>'
@@ -9,4 +10,15 @@ jQuery(document).ready(function($) {
     $(this).addClass('selected');
     return false;
   })
+
+  // handle items in cart with cached pages
+  $('li#cart_link').html('<a href="/cart/items">' + cart_text($.cookie('items_in_cart')) + '</a>');
 })
+
+function cart_text(item_count) {
+  if(item_count > 1) {
+    return item_count + ' items in cart';
+  } else {
+    return item_count + ' item in cart';
+  }
+}
