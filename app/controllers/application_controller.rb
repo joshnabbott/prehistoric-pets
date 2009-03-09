20e488c1cc18a4cc3c0de2fbae28211e1804b876
Joshua Abbott <joshnabbott@joshua-abbotts-macbook-pro.local>
2009-03-08 23:50:12 -0700

    changing the logic for cookie setting items_in_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class Error404 < StandardError; end
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
-  before_filter :find_order
+  before_filter :find_order, :find_items_in_cart
   helper :all # include all helpers, all the time
   rescue_from Error404, :with => :render_404_error
 
-  # protect_from_forgery :secret => '5547deee5dffa6aeb3678df88bb8aed5'
-
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password").
   filter_parameter_logging :password
+
 private
+  def find_items_in_cart
+    cookies[:items_in_cart] = @order.product_count
+  end
+
   def find_order
     if session[:order_reference_number].blank?
       @order = Order.create(:ip_address => request.remote_ip)
--- app/controllers/cart/items_controller.rb
+++ app/controllers/cart/items_controller.rb
@@ -1,12 +1,10 @@
 class Cart::ItemsController < Cart::CartController
-  after_filter :set_items_in_cart, :only => [ :create, :destroy ]
-
   def index
     respond_to do |format|
       format.html
     end
   end
-  
+
   def create
     @order.add_to_cart(Product.find(params[:product_id]), :quantity => params[:quantity])
 
@@ -23,10 +21,5 @@ class Cart::ItemsController < Cart::CartController
     respond_to do |format|
       format.html { redirect_to(cart_items_path) }
     end
-  end
-
-protected
-  def set_items_in_cart
-    cookies[:items_in_cart] = @order.product_count
   end
 end
