8b4f0a669ca0ac5a7c26f55278a8db11f9cc8859
Josh N. Abbott <joshnabbott@gmail.com>
2009-08-03 09:54:27 -0700

    don't send mail after db backup

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :path, '/var/www/prehistoricpets/current'
 
 every 1.day, :at => '2:00am' do
-  command "RAILS_ENV=production FILEPATH=/home/deploy/backups /usr/bin/ruby #{RAILS_ROOT}/script/dbdump --include-password | mail -s 'Prehistoric Database Backup' joshnabbott@gmail.com"
+  command "RAILS_ENV=production FILEPATH=/home/deploy/backups /usr/bin/ruby #{RAILS_ROOT}/script/dbdump --include-password"
 end
 
 every 4.hours do
