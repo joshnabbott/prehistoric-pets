00f0806bd80d33349e5689f943e982c96ae89a12
Josh N. Abbott <joshnabbott@gmail.com>
2009-08-03 09:30:34 -0700

    little mistake setting path for whenever. fixed.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :path, '/var/www/prehistoric/current'
+set :path, '/var/www/prehistoricpets/current'
 
 every 1.day, :at => '2:00am' do
   command "RAILS_ENV=production FILEPATH=/home/deploy/backups /usr/bin/ruby #{RAILS_ROOT}/script/dbdump --include-password | mail -s 'Prehistoric Database Backup' joshnabbott@gmail.com"
