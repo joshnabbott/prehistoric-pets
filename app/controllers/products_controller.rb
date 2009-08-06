8a1ca65a69eee72ef5d9d7252958a21c85b2b30b
Josh N. Abbott <joshnabbott@gmail.com>
2009-08-05 19:29:32 -0700

    fix missing template error for products#show in IE (removed unnecessary format.jpg from controller)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,6 @@ class ProductsController < ApplicationController
     raise Error404 unless @product.is_active
     respond_to do |format|
       format.html # show.html.erb
-      format.jpg # show.jpg.flexi
       format.xml  { render :xml => @product }
     end
   end
